3b8e916c72d791d1ff7b5e53ed9bb0a6403b5330
Tony <rideaction95@gmail.com>
2026-05-06 20:14:06 +0500

    h

--- main.cpp
+++ main.cpp
@@ -2,40 +2,56 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 #include <chrono>
 #include "note.h"
 #include <variant>
+#include <iomanip>
+#include <ctime>
 
 
 
-void createNote(std::vector<std::string>* main_v){
-    std::string description, tag;
-    std::getline(std::cin, description);
-    std::getline(std::cin, tag);
-    auto create_time = std::chrono::system_clock::now();
-    auto end_time = create_time + std::chrono::hours(24);
-    Status IN_PROGRESS;
-    std::getline(std::cin, description);
-    std::getline(std::cin, tag);
-    int id;
-    std::cin >> id;
-    std::vector<std::string> Note;
-    
-    
-
-
+void createNote(std::vector<Note> &storage){
+    Note note;
+    std::getline(std::cin, note.description);
+    std::getline(std::cin, note.tag);
+    note.create_time = std::chrono::system_clock::now();
+    note.end_time = note.create_time + std::chrono::hours(24);
+    note.status = Note::Status::IN_PROGRESS;       
+    storage.push_back(note);
 }
-void deleteNote(std::vector<std::string> &vectr){
-    vectr.clear();
+void deleteNote(std::vector<Note> &storage, int idex){
+    int size = storage.size();
+    for(int i = idex; i < size-1; ++i){
+        std::swap(storage[i], storage[i+1]);       
+    }
+    storage.pop_back();
 }
-void ShowAllNote(std::vector<std::string()){
+
+void ShowAllNote(const std::vector<Note> &storage){
+    int size = storage.size();
+    for(int i = 0; i < size; ++i){
+        std::cout << storage[i];
+    }
     
+}
+
+void showNote(const Note &note){
+    std::cout << note.tag << std::endl;
+    std::cout << note.description << std::endl;
+    std::time_t time = std::chrono::system_clock::to_time_t(note.create_time);
+    std::time_t time_end = std::chrono::system_clock::to_time_t(note.end_time);
+    auto now_time = std::chrono::system_clock::now();
+    auto left_time = std::chrono::duration_cast<std::chrono::hours>(note.end_time-now_time).count();
+    std::cout << "create time:" << std::ctime(&time) << std::endl;
+    std::cout << "time left:" <<
+
 }
 
 int main(){
-    std::vector<std::vector<std::string>> main_v;
-    createNote(&main_v);
+    std::vector<Note> storage;
+    createNote();
     
     return 0;
 }
--- note.h
+++ note.h
@@ -1,16 +1,19 @@
 #pragma once
 #include <string>
 #include <chrono>
-enum class Status {
-    IN_PROGRESS,
-    DONE,
-    END
-};
+
 struct Note {
+    enum class Status {
+        IN_PROGRESS,
+        DONE,
+        END
+    };
+    
     std::string tag; // типо работа офис чет такое 
     std::string description; // что сделать 
-    std::chrono::system_clock::time_point created;
-    std::chrono::system_clock::time_point end; 
-    unsigned int id;
+    std::chrono::system_clock::time_point create_time;
+    std::chrono::system_clock::time_point end_time; 
+    Status status;
+
 
 };
